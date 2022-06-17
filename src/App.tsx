9f79910c18acd359afebd876ad4976bdc736b804
Jeong yong hoon <jyh.dev@gmail.com>
2022-06-17 20:58:17 +0900

    UPDATE BASE PROJECT
    기존 프로젝트에서 필요없는 부분 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
-import {useState} from 'react'
 import {BrowserRouter as Router, Link, Route, Routes} from "react-router-dom";
 
-import './App.css'
 import HomePage from "./page/HomePage";
 import LoginPage from "./page/LoginPage";
 
 const App = () => {
-    const [count, setCount] = useState(0)
-
     //TODO: https://velog.io/@soryeongk/ReactRouterDomV6 (react-router-dom version 6에 대한 학습)
     //TODO: 현재 패키지 관리 툴은 yarn 가능하면 npm과 같이 쓰지 않기
 
@@ -22,43 +18,6 @@ const App = () => {
                 <Route path={"/test"} element={<div/>}/>
             </Routes>
         </Router>
-
-        // <div className="App">
-        //     Test
-        // </div>
-        // <div className="App">
-        //   <header className="App-header">
-        //     <img src={logo} className="App-logo" alt="logo" />
-        //     <p>Hello Vite + React!</p>
-        //     <p>
-        //       <button type="button" onClick={() => setCount((count) => count + 1)}>
-        //         count is: {count}
-        //       </button>
-        //     </p>
-        //     <p>
-        //       Edit <code>App.tsx</code> and save to test HMR updates.
-        //     </p>
-        //     <p>
-        //       <a
-        //         className="App-link"
-        //         href="https://reactjs.org"
-        //         target="_blank"
-        //         rel="noopener noreferrer"
-        //       >
-        //         Learn React
-        //       </a>
-        //       {' | '}
-        //       <a
-        //         className="App-link"
-        //         href="https://vitejs.dev/guide/features.html"
-        //         target="_blank"
-        //         rel="noopener noreferrer"
-        //       >
-        //         Vite Docs
-        //       </a>
-        //     </p>
-        //   </header>
-        // </div>
     )
 }
 
