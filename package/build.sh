0de6e0eb38b4354b813c4db3ee6d9a578f1bb115
Jeong yong hoon <jyh.dev@gmail.com>
2022-06-19 23:04:55 +0900

    DOCKER TEST
    images 생성 및 컨테이너 up 테스트

--- package/build.sh
+++ package/build.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
-cd ../
+
 rm -rf dist
+cd ../ || exit
 yarn install
 yarn build
+mv ./dist ./package/dist
+
+cd ./package || exit
+docker build -t drumbbang-client .
 rm -rf dist
 
 # Docker Image 띄우는 명령 추가하기
