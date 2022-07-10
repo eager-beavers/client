910d22f7e1b5851874447d99333e8b47286cc548
Jeong yong hoon <jyh.dev@gmail.com>
2022-07-10 22:18:13 +0900

    TEST CODE

--- src/page/LoginPage.tsx
+++ src/page/LoginPage.tsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const LoginPage = () => {
     return (
-        <>
+        <div className={"flex justify-center content-center h-full"}>
             <div className={"text-3xl font-bold underline"}>LoginPage</div>
-        </>
+        </div>
     )
 }
 export default LoginPage;
